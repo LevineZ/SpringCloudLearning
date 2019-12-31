2e7e5e652538144b61150e0d1df0836fe0cb8275
zhouhuan <zhouhuan@100doc.com.cn>
2019-12-31 16:41:24 +0800

    新增熔断器

--- Ribbon/src/main/java/com/ribbon/controller/RibbonController.java
+++ Ribbon/src/main/java/com/ribbon/controller/RibbonController.java
@@ -1,5 +1,6 @@
 package com.ribbon.controller;
 
+import com.ribbon.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,16 @@ public class RibbonController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    TestService testService;
+
     @GetMapping(value = "/add")
     public String add() {
         return restTemplate.getForEntity("http://LEVINE/add?a=10&b=20", String.class).getBody();
     }
 
+    @GetMapping(value = "add1")
+    public String add1() {
+        return testService.addService();
+    }
 }
