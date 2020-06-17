5abaff2c77b5bad303a1de7e9dcdb7c939a76abf
zhouhuan <zhouhuan@100doc.com.cn>
2020-06-17 11:41:09 +0800

    :sparkles:equals测试Demo

---
+++ test/src/main/java/test1/Test.java
@@ -0,0 +1,18 @@
+package test1;
+
+/**
+ * @Author: zhouhuan
+ * @Date: 2020/6/16 19:27
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        String a = null;
+        if ("Hello World".equals(a)) {
+            System.out.printf("OK");
+
+        } else {
+            System.out.printf("NO OK");
+        }
+    }
+}
